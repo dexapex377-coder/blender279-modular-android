fce24e2dac8936045c14e5442beeb8f6182215f0
root <root@localhost.localdomain>
2026-06-29 23:04:09 +0000

    Add BLI_math_inline.h and DNA_dynamicpaint_types.h stubs

---
+++ core/compat/BLI_math_inline.h
@@ -0,0 +1,6 @@
+#ifndef __BLI_MATH_INLINE_H__
+#define __BLI_MATH_INLINE_H__
+
+#include "BLI_math.h"
+
+#endif
---
+++ core/compat/DNA_dynamicpaint_types.h
@@ -0,0 +1,16 @@
+#ifndef __DNA_DYNAMICPAINT_TYPES_H__
+#define __DNA_DYNAMICPAINT_TYPES_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct DynamicPaintModifierData {
+    struct DynamicPaintModifierData *next, *prev;
+} DynamicPaintModifierData;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
