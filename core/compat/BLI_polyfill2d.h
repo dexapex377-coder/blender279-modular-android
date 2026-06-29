b25a36e0145f353936671eb452ecfbc5e3938647
root <root@localhost.localdomain>
2026-06-29 18:03:16 +0000

    Add BLI_polyfill2d.h with BLI_POLYFILL_ARENA_SIZE

--- core/compat/BLI_polyfill2d.h
+++ core/compat/BLI_polyfill2d.h
@@ -1,16 +1,24 @@
 #ifndef __BLI_POLYFILL2D_H__
 #define __BLI_POLYFILL2D_H__
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+#include "BLI_memarena.h"
 
-void BLI_polyfill_calc(const float (*coords)[2], const unsigned int coords_len, int (*tris)[3], unsigned int *r_tris_len);
-void BLI_polyfill_calc_arena(const float (*coords)[2], unsigned int coords_len, int (*tris)[3], unsigned int *r_tris_len, struct MemArena *arena);
-float BLI_polyfill_beautify_quad_rotate_calc(const float v1[2], const float v2[2], const float v3[2], const float v4[2]);
+struct MemArena;
 
-#ifdef __cplusplus
-}
-#endif
+void BLI_polyfill_calc_arena(
+        const float (*coords)[2],
+        const unsigned int coords_tot,
+        const int coords_sign,
+        unsigned int (*r_tris)[3],
+        struct MemArena *arena);
 
-#endif
+void BLI_polyfill_calc(
+        const float (*coords)[2],
+        const unsigned int coords_tot,
+        const int coords_sign,
+        unsigned int (*r_tris)[3]);
+
+/* default size of polyfill arena */
+#define BLI_POLYFILL_ARENA_SIZE MEM_SIZE_OPTIMAL(1 << 14)
+
+#endif  /* __BLI_POLYFILL2D_H__ */
