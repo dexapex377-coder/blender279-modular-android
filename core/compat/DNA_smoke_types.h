3bbf729c212650fce786f597bbc34402ef8ad37f
root <root@localhost.localdomain>
2026-06-30 04:57:51 +0000

    Add more SmokeDomain/FlowSettings fields

--- core/compat/DNA_smoke_types.h
+++ core/compat/DNA_smoke_types.h
@@ -7,13 +7,22 @@
 extern "C" {
 #endif
 
+struct Group;
+struct EffectorWeights;
+struct Tex;
+
 typedef struct SmokeDomainSettings {
+    struct Group *fluid_group;
+    struct Group *eff_group;
+    struct Group *coll_group;
+    struct EffectorWeights *effector_weights;
     int dummy;
 } SmokeDomainSettings;
 
 typedef struct SmokeFlowSettings {
     int source;
     int texture_type;
+    struct Tex *noise_texture;
     char vgroup_density[64];
     char vgroup_name[64];
     int flags;
