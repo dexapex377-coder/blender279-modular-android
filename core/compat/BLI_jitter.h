10c6aa7a716640f444b62f4dcb7d53b89dbaffaa
root <root@localhost.localdomain>
2026-06-30 06:04:07 +0000

    Add GPU_buffers.h, BLI_jitter.h

---
+++ core/compat/BLI_jitter.h
@@ -0,0 +1,14 @@
+#ifndef __BLI_JITTER_H__
+#define __BLI_JITTER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void BLI_jitter_init(float *jit, int num);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ core/compat/GPU_buffers.h
@@ -0,0 +1,15 @@
+#ifndef __GPU_BUFFERS_H__
+#define __GPU_BUFFERS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void GPU_buffers_free(void);
+void GPU_buffers_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
