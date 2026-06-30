d1ffebe813a45c939f39a7d27e815573316f8f72
root <root@localhost.localdomain>
2026-06-30 04:50:45 +0000

    Add DNA_smoke_types.h

---
+++ core/compat/DNA_smoke_types.h
@@ -0,0 +1,18 @@
+#ifndef __DNA_SMOKE_TYPES_H__
+#define __DNA_SMOKE_TYPES_H__
+
+#include "DNA_ID.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct SmokeModifierData {
+    struct ID id;
+} SmokeModifierData;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
