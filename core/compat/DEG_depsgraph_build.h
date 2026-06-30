8bc1b28d41a84eb5fa48397a5e2bdb03946f5a7f
root <root@localhost.localdomain>
2026-06-30 05:34:33 +0000

    Add DEG component enums

--- core/compat/DEG_depsgraph_build.h
+++ core/compat/DEG_depsgraph_build.h
@@ -5,6 +5,12 @@
 extern "C" {
 #endif
 
+enum {
+    DEG_OB_COMP_TRANSFORM = 0,
+    DEG_OB_COMP_EVAL_POSE = 1,
+    DEG_OB_COMP_GEOMETRY = 2,
+};
+
 void DEG_add_build_relation(struct DepsRelation *relation);
 void DEG_graph_build_finalize(struct Depsgraph *graph);
 
